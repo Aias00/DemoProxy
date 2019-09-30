098756d7c03247814bddade2398b5e06416859cd
aias00 <rokkki@163.com>
2019-09-30 19:50:11 +0800

    更新代码依赖

--- src/main/java/com/aias/demo/custom/Client.java
+++ src/main/java/com/aias/demo/custom/Client.java
@@ -1,7 +1,5 @@
 package com.aias.demo.custom;
 
-import com.aias.demo.jdk.UserService;
-
 public class Client {
     public static void main(String[] args) {
         EnglishTeacher teacher = new EnglishTeacher();
